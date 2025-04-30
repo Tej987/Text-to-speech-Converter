f1487729ee24695a36705b3d6ac55d1b567f4234
Tej987 <128631796+Tej987@users.noreply.github.com>
2025-04-30 23:23:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function speak() {
+    const text = document.getElementById("text").value;
+    const speech = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(speech);
+  }
+  
